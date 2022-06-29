66f503d0d6b0aa2389cf7ef7576cb308fee5907a
Rishabh Pandey <email.rishabhp@gmail.com>
2022-06-28 19:28:30 -0700

    Added countdown and resized frame

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import cv2 as cv
 import numpy as np
 import random
-'''
+from datetime import datetime
+
+''' 
+TASKS + STATUS
 - Open laptop camera/webcam ------------------------------------------------------- DONE
 - Turn live-feed into grayscale --------------------------------------------------- DONE
-- Resize frame dimensions 
-- Add textbox countdown from 5
+- Resize frame dimensions --------------------------------------------------------- Check dimensions with shyam
+- Add textbox countdown from 5 ---------------------------------------------------- DONE
 - After 5 seconds, take camera snapshot and save it in the testing_photo folder --- DONE
 '''
 
@@ -17,13 +20,20 @@ if not video.isOpened():
 
 new_filename = (str) (random.random())[2:9] + '.png'
 
+start_time = datetime.now()
+print(f'Start time is: {start_time}.')
+
 while True:
     ret, frame = video.read()
-    # cv.resize(frame, ())
-    # Ask shyam about dimensions
+    cv.resize(frame, (600,600))
+    cv.putText(frame, f"Time left until picture taken: {10 - ((datetime.now() - start_time).seconds)}", (50,50), cv.FONT_HERSHEY_SIMPLEX, 1.0, (255,0,0))
     gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     cv.imshow('Camera', gray_frame)
     if cv.waitKey(1) == ord('a'):
+        cv.imwrite('image_testing/' + new_filename, gray_frame)
+        print(f'Image {new_filename} saved to testing folder!')
+        break
+    if (10 - ((datetime.now() - start_time).seconds)) < 1:
         cv.imwrite('image_testing/' + new_filename, gray_frame)
         print(f'Image {new_filename} saved to testing folder!')
         break
